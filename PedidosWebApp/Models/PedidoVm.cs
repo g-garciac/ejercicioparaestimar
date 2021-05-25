acc3cfa723f6521d42bd829fb804a29d60e9bf02
Gerardo Garcia <g.garciac@outlook.com>
2021-05-24 22:28:15 -0500

    View model

---
+++ PedidosWebApp/Models/PedidoVm.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PedidosWebApp.Models
+{
+    public class PedidoVm
+    {
+        public string CodigoABuscar { get; set; }
+        public decimal CantidadAgregar { get; set; }
+        public IEnumerable<ProductoPedidoVm> Productos { get; set; }
+        public decimal Total => Productos.Sum(p => p.Importe);
+    }
+}
---
+++ PedidosWebApp/Models/ProductoPedidoVm.cs
@@ -0,0 +1,11 @@
+﻿namespace PedidosWebApp.Models
+{
+    public class ProductoPedidoVm
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Importe => Cantidad * Precio;
+    }
+}
